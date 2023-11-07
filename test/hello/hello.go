9e31c6422d9edf836f312c365f2570600c0d9540
YoungAD <aerdaxyoung@gmail.com>
2023-11-07 18:47:21 +0800

    init project

---
+++ test/hello/hello.go
@@ -0,0 +1,27 @@
+package hello
+
+const spanish = "Spanish"
+const french = "French"
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+
+func Hello(name, language string) string {
+	if name == "" {
+		name = "World"
+	}
+	prefix, _ := greetingPrefix(language)
+	return prefix + name
+}
+
+func greetingPrefix(language string) (prefix, msg string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+}
